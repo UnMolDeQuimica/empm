a948ce8524d67094be59527f01d68617d51cb128
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-12-30 11:33:30 +0100

    feat: Added basic views
    Still not working as we require more views here to make the links work

--- main/views.py
+++ main/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, View
+
+
+class Home(TemplateView):
+    template_name = "index.html"
+
+
+class HowDoesItWork(TemplateView):
+    template_name = "how_does_it_work.html"
+
+
+class CookiesPolicy(TemplateView):
+    template_name = "cookies_policy.html"
+
